fad97782cafacd9411f873530c31e58acbc00895
hasip <hasifpriyambudi13@gmail.com>
2023-06-10 11:33:58 +0700

    menambah tombol ekspor

--- app/Views/admin/laporan-penjualan.php
+++ app/Views/admin/laporan-penjualan.php
@@ -50,7 +50,12 @@
         </tbody>
       </table>
     </div>
-    <div class="col-md-12">
+
+    <div class="col-md-6 col-xs-6">
+      <button type="button" class="btn btn-success" title="Ekspor Excel"><i class="fa-solid fa-file-export"></i> Ekspor</button>
+    </div>
+
+    <div class="col-md-6 col-xs-6">
       <nav aria-label="..." style="float:right;">
         <ul class="pagination">
           <li class="page-item disabled">
